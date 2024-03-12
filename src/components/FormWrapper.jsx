afa5c4cb7f13ef52a4a544a1915ecee467857093
Yohann Andre <y.andre90000@gmail.com>
2024-03-12 17:30:52 +0100

    add new form choices

---
+++ src/components/FormWrapper.jsx
@@ -0,0 +1,36 @@
+import React, { forwardRef, useEffect } from "react";
+
+const FormWrapper = forwardRef(({ array, setter, isOpen, setIsOpen }, ref) => {
+  FormWrapper.displayName = FormWrapper;
+
+  useEffect(() => {
+    let handler = (e) => {
+      if (!ref.current.contains(e.target)) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handler);
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    };
+  }, [isOpen]);
+
+  return (
+    <div ref={ref} className="absolute z-20 flex w-full flex-col bg-slate-600">
+      {array.map((e, index) => (
+        <p
+          key={index}
+          className="pl-1 text-black hover:bg-blue-500"
+          onClick={() => {
+            setter(e);
+            setIsOpen(false);
+          }}
+        >
+          {e}
+        </p>
+      ))}
+    </div>
+  );
+});
+
+export default FormWrapper;
